96b2cf16f819ca08e1b44e60d945b7d9abec35f6
Vivek Tyagi <tyagivivek435@gmail.com>
2025-07-28 00:04:28 +0530

    implement ai companion

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CompanionComponents from "@/components/CompanionComponents";
 import { getCompanion } from "@/lib/actions/companion.actions";
 import { getSubjectColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
@@ -11,8 +12,9 @@ interface CompanionSessionPageProps {
 
 const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
   const { id } = await params;
-  const { name, topic, subject, duration, title } = await getCompanion(id);
+  const companion = await getCompanion(id);
   const user = await currentUser();
+  const { name, topic, subject, duration, title } = companion;
 
   if (!user) redirect("/sign-in");
   if (!name) redirect("/companion");
@@ -45,6 +47,12 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
           {duration} minutes
         </div>
       </article>
+      <CompanionComponents
+        {...companion}
+        companionId={id}
+        userName={user.firstName!}
+        userImage={user.imageUrl!}
+      />
     </main>
   );
 };
